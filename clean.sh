a0f2b66381ab52ae0036039c4454730f6cb97c69
Pratyush Das <reikdas@gmail.com>
2023-04-16 19:52:54 -0400

    Add MPI setup

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./stop.sh
 
-docker rm -vf $(docker ps --filter ancestor=pash-base -aq);
+docker rm -vf $(docker ps --filter ancestor=hadoop-pash-base -aq);
 docker rmi -f $(docker images --format "{{.Repository}} {{.ID}}" | cut -d " " -f2)
 
 echo "Warning: you will need to run image and volume on every swarm node"
